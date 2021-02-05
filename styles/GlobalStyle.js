88e6cc51ab14461810c8c7b83ccd9dce52892cca
Lukas Kreidenweis <lukas@deinklub.de>
2021-02-05 20:09:30 +0100

    add some global styling

--- styles/GlobalStyle.js
+++ styles/GlobalStyle.js
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
   html {
     --color-text: white;
     --color-primary: rgb(6,15,20);
+    --color-primary-transparent: rgb(6,15,20,0.9);
     --font-family-primary: Arial, Helvetica, sans-serif;
     --font-family-secondary: 'Times New Roman', Times, serif;
 
@@ -19,6 +20,7 @@ const GlobalStyle = createGlobalStyle`
   }
   
   body {
+    width: 100%;
     padding: 0;
     margin: 0;
     background-color: var(--color-primary);
@@ -37,6 +39,29 @@ const GlobalStyle = createGlobalStyle`
 
   p {
     margin: 0;
+    font-size: 1.2rem;
+  }
+
+  h1, h2 {
+    font-family: var(--font-family-secondary);
+  }
+
+  h1 {
+    font-size: 3.8rem;
+    line-height: 1.1;
+    margin-bottom: 2rem;
+
+    @media screen and (min-width: 820px) {
+      font-size: 4rem;
+    }
+  }
+
+  h2 {
+    font-size: 2.8rem;
+
+    @media screen and (min-width: 820px) {
+      font-size: 2rem;
+    }
   }
 `;
 
