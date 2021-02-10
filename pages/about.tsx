9f1ffa66be4923e62ea962ffd1e08baf11a5c2dd
Lukas Kreidenweis <lukas@deinklub.de>
2021-02-10 18:50:26 +0100

    shuffle teamMembers on serverside

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,33 +1,23 @@
+import { GetStaticProps } from "next";
 import NextImage from "next/image";
 import shuffleArray from "services/shuffleArray";
 import styled from "styled-components";
 
-const teamMembers = [
-  {
-    name: "Cornelius Bierer",
-    video: {
-      mp4: { url: "/placeholder/Cornelius-Bierer_loop.mp4" },
-      webm: { url: "/placeholder/Cornelius-Bierer_loop.webm" },
-    },
-    image: { url: "/placeholder/Cornelius-Bierer_loop.png", alt: "" },
-  },
-  {
-    name: "Matthias Bierer",
-    video: {
-      mp4: { url: "/placeholder/Matthias-Bierer_loop.mp4" },
-      webm: { url: "/placeholder/Matthias-Bierer_loop.webm" },
-    },
-    image: { url: "/placeholder/Matthias-Bierer_loop.png", alt: "" },
-  },
-];
+type AboutProps = {
+  teamMembers: [
+    {
+      name: string;
+      video: { mp4: { url: string }; webm: { url: string } };
+      image: { url: string; alt: string };
+    }
+  ];
+};
 
-export default function About() {
-  const shuffledTeamMembers = shuffleArray(teamMembers);
-
+export default function About({ teamMembers }: AboutProps) {
   return (
     <PageWrapper>
       <Container>
-        {shuffledTeamMembers.map(({ name, video, image }, index) => (
+        {teamMembers.map(({ name, video, image }, index) => (
           <Wrapper key={index}>
             <Name>{name}</Name>
             <Video autoPlay loop muted playsInline>
@@ -45,13 +35,44 @@ export default function About() {
       </Container>
       <TextContainer>
         <h1>About</h1>
+        <p>Wir sind Brüder seit 1989, und Filmemacher seit 2012.</p>
         <p>
-          Wir sind Brüder, und Brüder streiten. Vor Jahren mussten wir uns ein
-          Zimmer teilen, heute teilen wir unsere Leidenschaft für Film und Musik
-          durch wemake. Bei unserer gegenseitigen Kritik nehmen wir kein Blatt
-          vor den Mund, außerdem ist am Ende mindestens einer von uns ein
-          unnachgiebiger Perfektionist. All das kommt letztendlich unseren
-          Kunden zugute.
+          Unsere Arbeit verändert den Blick auf die Dinge. Und ab und an sogar
+          das Leben*. Als Filmemacher interessiert uns vor allem, wie eine
+          Geschichte erzählt wird und warum - und was man dabei denkt und fühlt.
+          Wir produzieren ehrlichen Content mit der Ambition, Ästhetik und
+          Aussage bestmöglich zu verschmelzen. Durch echte Inhalte, starke
+          Bilder, wirkungsvolle Musik und spürbare Sounds transportieren wir
+          Informationen und Emotionen kreativ an ihr Ziel. Egal wie die
+          Herangehensweise aussieht, am Ende steht ein stimmiges,
+          stimmungsvolles und authentisches Ergebnis. Und eine Geschichte, die
+          gehört werden will.
+        </p>
+        <p>
+          <small>
+            *2018 Crowdfunding-Video für Gebrüder Udsilauri
+            <br />
+            {"->"}{" "}
+            <a href="https://www.facebook.com/daniel.udsilauri?__cft__[0]=AZXMWd68_VdZc240URUEqmPS8cOi6XxOO2ZHvJRPO2ozUXz4sdUgKHAuQLASTNPowdK1q_Qrf3jWf75p9gqpcd4cMOpR6uRh2HNmEBA6v41_NOuc12NNtAUNCJyN5PV1ot2m9NY5dNLd3NgcCa8ccnpX6pxi25dZpS5UqnVgOTXBjg&__tn__=-]K-R">
+              Daniel Udsilauri
+            </a>{" "}
+            wird Deutscher Meister 2019 im U18-Judo
+          </small>
+        </p>
+        <p>
+          <small>
+            {" "}
+            *2019 Kampagnen-Video Europawahlkampf für{" "}
+            <a href="https://www.facebook.com/freundaniel?__cft__[0]=AZXMWd68_VdZc240URUEqmPS8cOi6XxOO2ZHvJRPO2ozUXz4sdUgKHAuQLASTNPowdK1q_Qrf3jWf75p9gqpcd4cMOpR6uRh2HNmEBA6v41_NOuc12NNtAUNCJyN5PV1ot2m9NY5dNLd3NgcCa8ccnpX6pxi25dZpS5UqnVgOTXBjg&__tn__=-]K-R">
+              Daniel Freund
+            </a>
+            <br />
+            {"->"} Daniel Freund zieht zusammen mit 20 anderen von{" "}
+            <a href="https://www.facebook.com/B90DieGruenen/?rf=1837152016509293">
+              Bündnis90/DieGrünen
+            </a>{" "}
+            ins Europaparlament ein
+          </small>
         </p>
       </TextContainer>
       <TextContainer>
@@ -60,6 +81,31 @@ export default function About() {
     </PageWrapper>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const teamMembers = [
+    {
+      name: "Cornelius Bierer",
+      video: {
+        mp4: { url: "/placeholder/Cornelius-Bierer_loop.mp4" },
+        webm: { url: "/placeholder/Cornelius-Bierer_loop.webm" },
+      },
+      image: { url: "/placeholder/Cornelius-Bierer_loop.png", alt: "" },
+    },
+    {
+      name: "Matthias Bierer",
+      video: {
+        mp4: { url: "/placeholder/Matthias-Bierer_loop.mp4" },
+        webm: { url: "/placeholder/Matthias-Bierer_loop.webm" },
+      },
+      image: { url: "/placeholder/Matthias-Bierer_loop.png", alt: "" },
+    },
+  ];
+
+  const shuffledTeamMembers = shuffleArray(teamMembers);
+
+  return { props: { teamMembers: shuffledTeamMembers } };
+};
 
 const PageWrapper = styled.div`
   display: grid;
@@ -128,5 +174,9 @@ const TextContainer = styled.div`
 
   @media screen and (min-width: 820px) {
     max-width: 1200px;
+  }
+
+  p {
+    margin-bottom: 1rem;
   }
 `;
