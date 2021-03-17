4dde808049ed9b6b560d64e3a8b4b4fd6dc4d6fb
Lukas Kreidenweis <lukas@deinklub.de>
2021-03-17 12:34:25 +0100

    refactor code

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,9 @@
 import { GetStaticProps } from "next";
-import NextImage from "next/image";
 import shuffleArray from "services/shuffleArray";
 import styled from "styled-components";
 import requestGraphCMS from "services/graphcms";
 import DOMPurify from "isomorphic-dompurify";
+import TeamMembers from "components/TeamMembers";
 
 type AboutProps = {
   teamMembers: [
@@ -25,24 +25,7 @@ export default function About({ teamMembers, aboutPage }: AboutProps) {
 
   return (
     <PageWrapper>
-      <Container>
-        {teamMembers.map(({ name, videos, placeholderImage }, index) => (
-          <Wrapper key={index}>
-            <Name>{name}</Name>
-            <Video autoPlay loop muted playsInline>
-              {videos.map(({ url, mimeType }, index) => (
-                <source key={index} src={url} type={mimeType} />
-              ))}
-              <NextImage
-                src={placeholderImage.url}
-                alt={placeholderImage.alt}
-                layout="fill"
-                quality={100}
-              />
-            </Video>
-          </Wrapper>
-        ))}
-      </Container>
+      <TeamMembers teamMembers={teamMembers} />
       <TextContainer>
         <h1>{header}</h1>
         <TextContainer
@@ -91,53 +74,6 @@ const PageWrapper = styled.div`
   @media screen and (min-width: 600px) {
     padding-top: 30vh;
   }
-`;
-
-const Container = styled.div`
-  display: grid;
-  grid-template-rows: 1fr 1fr;
-  grid-gap: 6rem;
-  width: 80vw;
-  margin: 0 auto;
-
-  @media screen and (min-width: 820px) {
-    grid-template-columns: 1fr 1fr;
-    height: 70vh;
-    max-width: 1200px;
-  }
-`;
-
-const Wrapper = styled.div`
-  position: relative;
-  width: 100%;
-  height: 50vh;
-
-  @media screen and (min-width: 820px) {
-    height: 80vh;
-
-    :last-of-type {
-      margin: 6rem 0 -6rem;
-    }
-  }
-`;
-
-const Name = styled.p`
-  position: absolute;
-  text-align: center;
-  z-index: 10;
-  transform: rotate(-90deg) translate(-50%, -50%);
-  transform-origin: bottom left;
-  bottom: 50%;
-  max-width: 70vh;
-`;
-
-const Video = styled.video`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  object-position: center center;
-  z-index: 5;
 `;
 
 const TextContainer = styled.div`
