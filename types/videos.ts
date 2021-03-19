5961bcc5005ce54649c621b07b6203fc2f6f1904
Lukas Kreidenweis <lukas@deinklub.de>
2021-03-19 19:05:26 +0100

    modify video type

--- types/videos.ts
+++ types/videos.ts
@@ -1,9 +1,12 @@
 export type Video = {
   title: string;
+  vimeoUrl: string;
   descriptionTitle?: string;
   description?: string;
-  vimeoUrl: string;
   randomMargin?: number;
+  customer?: {
+    name: string;
+  };
 };
 
 export type Videos = [Video];
