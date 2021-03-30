1ea0e813f7105227afb068b4ac7ac47c62422766
Lukas Kreidenweis <lukas@deinklub.de>
2021-03-30 20:40:47 +0200

    embed fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import MainLayout from "components/MainLayout";
 import type { AppProps } from "next/app";
 import GlobalStyle from "styles/GlobalStyle";
+import "../styles/fonts.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
