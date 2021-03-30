69c13ffd9c9faa7dfd8c6776dc2ab2ae5ccffc6f
Lukas Kreidenweis <lukas@deinklub.de>
2021-03-30 20:37:08 +0200

    add new fonts modify global styling

--- styles/GlobalStyle.js
+++ styles/GlobalStyle.js
@@ -9,9 +9,9 @@ const GlobalStyle = createGlobalStyle`
 
   html {
     --color-text: white;
-    --color-primary: rgb(6,15,20);
+    --color-primary: 6,15,20;
     --color-primary-transparent: rgb(6,15,20,0.9);
-    --font-family-primary: 'Karla', sans-serif;
+    --font-family-primary: 'Roboto', sans-serif;
     --font-family-secondary: 'Spectral SC', serif;
     --max-content-width: 1200px;
 
@@ -24,7 +24,7 @@ const GlobalStyle = createGlobalStyle`
     width: 100%;
     padding: 0;
     margin: 0;
-    background-color: var(--color-primary);
+    background-color: rgb(var(--color-primary));
   }
 
   a {
@@ -32,30 +32,29 @@ const GlobalStyle = createGlobalStyle`
     color: var(--color-text);
     text-decoration: none;
     cursor: pointer;
-
-    &.active {
-      font-style: italic;
-    }
+    font-style: italic;
+    text-transform: lowercase;
   }
 
   p {
     margin: 0;
     font-size: 1.125rem;
+    line-height: 1.75rem;
     font-weight: 300;
-    line-height: 1.5rem;
-
-    @media screen and (min-width: 820px) {
-    font-size: 1.25rem;}
+    letter-spacing: 0.3px;
   }
 
   h1, h2, h3 {
     font-family: var(--font-family-secondary);
+    font-weight: unset;
+    font-style: italic;
   }
 
   h1 {
     font-size: 14vmin;
-    line-height: 1.1;
-    margin: 8vmin 0 4vmin;
+    line-height: 1.0;
+    margin-bottom: 1rem;
+    text-transform: uppercase;
 
     @media screen and (min-width: 420px) {
       font-size: 10vmin;
@@ -68,20 +67,24 @@ const GlobalStyle = createGlobalStyle`
 
   h2 {
     font-size: 10vmin;
-    margin: 6vmin 0 3vmin;
+    margin: 6vmin 0 1rem;
+    text-transform: uppercase;
+    padding-left: 2rem;
 
     @media screen and (min-width: 420px) {
       font-size: 8vmin;
     }
 
     @media screen and (min-width: 820px) {
-      font-size: 2.2rem;
+      font-size: 2rem;
     }
   }
 
   h3 {
     font-size: 8vmin;
-    margin: 4vmin 0 2vmin;
+    margin: 4vmin 0 1rem;
+    text-transform: uppercase;
+    padding-left: 2rem;
 
     @media screen and (min-width: 420px) {
       font-size: 6vmin;
@@ -92,14 +95,7 @@ const GlobalStyle = createGlobalStyle`
     }
   }
 
-  small  {
-    font-size: 0.8rem;
-  }
-
-  .small > p  {
-    font-size: 0.8rem;
-    margin-bottom: 0;
-  }
+  
 
 `;
 
