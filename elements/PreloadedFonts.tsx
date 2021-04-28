8117b0fb61aeebd168163f9f2b21db8150ee7efb
Lukas Kreidenweis <lukas@deinklub.de>
2021-04-28 16:04:58 +0200

    update font preload for Chrome on IOS

--- elements/PreloadedFonts.tsx
+++ elements/PreloadedFonts.tsx
@@ -2,13 +2,21 @@ const isBrowser = typeof window !== "undefined";
 
 const fonts = [
   { href: "/fonts/Roboto/Roboto-Light.woff2", type: "font/woff2" },
+  { href: "/fonts/Roboto/Roboto-Light.ttf", type: "font/ttf" },
   { href: "/fonts/Roboto/Roboto-LightItalic.woff2", type: "font/woff2" },
+  { href: "/fonts/Roboto/Roboto-LightItalic.ttf", type: "font/ttf" },
   { href: "/fonts/Roboto/Roboto-Regular.woff2", type: "font/woff2" },
+  { href: "/fonts/Roboto/Roboto-Regular.ttf", type: "font/ttf" },
   { href: "/fonts/Roboto/Roboto-Italic.woff2", type: "font/woff2" },
+  { href: "/fonts/Roboto/Roboto-Italic.ttf", type: "font/ttf" },
   { href: "/fonts/Roboto/Roboto-Bold.woff2", type: "font/woff2" },
+  { href: "/fonts/Roboto/Roboto-Bold.ttf", type: "font/ttf" },
   { href: "/fonts/Roboto/Roboto-BoldItalic.woff2", type: "font/woff2" },
+  { href: "/fonts/Roboto/Roboto-BoldItalic.ttf", type: "font/ttf" },
   { href: "/fonts/Spectral/Spectral-Light.woff2", type: "font/woff2" },
+  { href: "/fonts/Spectral/Spectral-Light.ttf", type: "font/ttf" },
   { href: "/fonts/Spectral/Spectral-LightItalic.woff2", type: "font/woff2" },
+  { href: "/fonts/Spectral/Spectral-LightItalic.ttf", type: "font/ttf" },
 ];
 
 function PreloadedFonts() {
