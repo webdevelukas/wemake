e6a05dfbbfe50bbd6ab37784e642a655d28bc406
Lukas Kreidenweis <lukas@deinklub.de>
2021-05-06 14:19:59 +0200

    add redirects for old links

--- next.config.js
+++ next.config.js
@@ -2,4 +2,33 @@ module.exports = {
   images: {
     domains: ["i.vimeocdn.com", "media.graphcms.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/portfolio/:slug*",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/items/:slug*",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/category/:slug*",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/about-us/:slug*",
+        destination: "/about",
+        permanent: true,
+      },
+      {
+        source: "/wemake/:slug*",
+        destination: "/about",
+        permanent: true,
+      },
+    ];
+  },
 };
